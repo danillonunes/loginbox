3c9b001e464d5e32fd7d2c3af1a8c75f92284055
Danillo Nunes <git@danillonunes.net>
2012-08-08 18:22:51 -0300

    Add extensible overlay animation function

--- js/loginbox.overlay.js
+++ js/loginbox.overlay.js
@@ -1,4 +1,9 @@
 /**
+ * Initialize global Login-box Overlay variable.
+ */
+Drupal.loginBox.overlay = Drupal.loginBox.overlay || {};
+
+/**
  * Add overlay under Login-box.
  */
 Drupal.behaviors.loginboxOverlay = function(context) {
@@ -8,10 +13,93 @@ Drupal.behaviors.loginboxOverlay = function(context) {
   $overlay.insertBefore($loginbox);
 
   Drupal.loginBox.beforeOpen.overlay = function() {
-    $overlay.show();
+    var settings = Drupal.loginBox.overlay.settings();
+    if ($.isFunction(settings.open.start)) {
+      settings.open.start.call($overlay);
+    }
+    $overlay.animate(settings.open.animate.properties, settings.open.animate.options);
   };
 
   Drupal.loginBox.beforeClose.overlay = function() {
-    $overlay.hide();
+    var settings = Drupal.loginBox.overlay.settings();
+    if ($.isFunction(settings.close.start)) {
+      settings.close.start.call($overlay);
+    }
+    $overlay.animate(settings.close.animate.properties, settings.close.animate.options);
   };
+};
+
+/**
+ * Return Login-box Overlay default settings.
+ *
+ * The overlay is faded in/out by default.
+ */
+Drupal.loginBox.overlay.defaultSettings = function() {
+  var duration = 100,
+      settings = {
+        open: {
+
+          /**
+           * This function is called before the animation starts.
+           */
+          start: function() {
+            $(this)
+              .css({
+                display: 'block',
+                opacity: 0
+              });
+          },
+
+          /**
+           * The animate properties and options.
+           *
+           * @see http://api.jquery.com/animate
+           */
+          animate: {
+            properties: {
+              opacity: 1
+            },
+            options: {
+              duration: duration
+            }
+          }
+        },
+        close: {
+
+          /**
+           * This function is called before the animation starts.
+           */
+          start: function() {},
+
+          /**
+           * The animate properties and options.
+           *
+           * @see http://api.jquery.com/animate
+           */
+          animate: {
+            properties: {
+              opacity: 0
+            },
+            options: {
+              duration: duration,
+              complete: function() {
+                $(this).hide()
+              }
+            }
+          }
+        }
+      };
+
+  return settings;
+};
+
+/**
+ * Return Login-box Overlay settings.
+ */
+Drupal.loginBox.overlay.settings = function() {
+  var settings = Drupal.settings.loginBox || {},
+      settingsOverlay = settings.overlay || {},
+      defaultSettings = Drupal.loginBox.overlay.defaultSettings();
+
+  return $.extend(true, {}, defaultSettings, settingsOverlay);
 };
